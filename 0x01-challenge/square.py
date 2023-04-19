4fa91a3fa0103a25a2287ac320fd03177df110c9
Emani23paulo <112129238+Emani23paulo@users.noreply.github.com>
2023-04-19 07:08:39 +0100

    0x01-challenge

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square():
+    """Square class."""
+
+    def __init__(self, *args, **kwargs):
+        """Init sequence."""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def perimeter(self):
+        """ Perimeter of the square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """String representation."""
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area())
+    print(s.perimeter())
